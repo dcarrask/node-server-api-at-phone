c35092ed167ed40c08a978aa5904997b88f04304
Diego Carrasco <dcarrask@gmail.com>
2020-10-19 20:54:09 +0200

    updating to get lan enpoints calls

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ const PORT = process.env.PORT || 8080;
 console.log(`server.js ### PORT:\n${JSON.stringify(PORT, null, 2)}`);
 
 // var server = app.listen(PORT, '0.0.0.0', function () {
-var server = app.listen(PORT, '192.168.1.1', function () {
+// var server = app.listen(PORT, '192.168.1.1', function () {
+// var server = app.listen(config.port, '0.0.0.0', function () {    
+var server = app.listen(PORT, '0.0.0.0', function () {
 	console.log(`App now running on => http://${process.env.DOMAIN}:${PORT}\n`);
 });
